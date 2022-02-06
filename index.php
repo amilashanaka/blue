24a99de4de2b025f928a0c9689a3172767fa3fb2
Amila Shanka Gunashinha <dsa.amilashanaka@gmail.com>
2022-02-06 12:58:45 +0000

    update

--- index.php
+++ index.php
@@ -3,20 +3,22 @@
 <head>
 	<title>Blue Telecoms</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-<!--    <link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">-->
+    <link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-	<img class="wave" src="assets/img/wave.png">
-	<div class="container">
-		<div class="img">
-			<img src="assets/img/bg.svg">
+<video autoplay muted loop class="myVideo"  style="z-index:0" >
+    <source src="assets/video/bg.mp4" type="video/mp4">
+</video>
+	<div class="container"  >
+		<div class="img" >
+			<img   src="assets/img/blue-telecoms-2020-v1.png">
 		</div>
 		<div class="login-content">
 			<form action="index.html">
-				<img src="assets/img/avatar.svg">
+				<img src="assets/img/user.png" style="z-index: -1">
 				<h2 class="title">Welcome</h2>
            		<div class="input-div one">
            		   <div class="i">

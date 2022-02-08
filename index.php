b101f5438d7884e4902eba805e55a58d84844644
Amila Shanka Gunashinha <dsa.amilashanaka@gmail.com>
2022-02-08 05:58:49 +0000

    update

--- index.php
+++ index.php
@@ -1,52 +1,55 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Blue Telecoms</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Blue Telecoms</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
-	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
 </head>
 <body>
 
 <div class="vh-100 w-100 d-sm-flex justify-content-start">
-  <div class="position-relative  d-none d-sm-block">
-  		<img class="w-100" src="assets/img/bg6.jpg">
-		<div class="position-absolute top-0 d-flex justify-content-end px-3">
-			<img src="assets/img/blue-telecoms-2020-v1.png" class="w-50">
-		</div>
-  </div>
-  <div class="login-content d-flex justify-content-center align-items-center px-4">
-  		<form action="controllers/login.php">
-				<img src="assets/img/user.png">
-				<h2 class="title">Welcome</h2>
-           		<div class="input-div one">
-           		   <div class="i">
-           		   		<i class="fas fa-user"></i>
-           		   </div>
-           		   <div class="div">
-           		   		<h5>Username</h5>
-           		   		<input type="text" class="input" name="u_id" id="u_id">
-           		   </div>
-           		</div>
-           		<div class="input-div pass">
-           		   <div class="i"> 
-           		    	<i class="fas fa-lock"></i>
-           		   </div>
-           		   <div class="div">
-           		    	<h5>Password</h5>
-           		    	<input type="password" class="input">
-            	   </div>
-            	</div>
-            	<a href="#">Forgot Password?</a>
-            	<input type="submit" class="btn text-white" value="Login">
-            </form>
-  </div>
+    <div class="position-relative  d-none d-sm-block">
+        <img class="w-100" src="assets/img/bg6.jpg">
+        <div class="position-absolute top-0 d-flex justify-content-end px-3">
+            <img src="assets/img/blue-telecoms-2020-v1.png" class="w-50 animate__animated animate__bounceInLeft">
+        </div>
+    </div>
+    <div class="login-content d-flex justify-content-center align-items-center px-4">
+        <form action="controllers/login.php">
+            <img src="assets/img/user.png">
+            <h2 class="title">Welcome</h2>
+            <div class="input-div one">
+                <div class="i">
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="div">
+                    <h5>Username</h5>
+                    <input type="text" class="input" name="u_id" id="u_id">
+                </div>
+            </div>
+            <div class="input-div pass">
+                <div class="i">
+                    <i class="fas fa-lock"></i>
+                </div>
+                <div class="div">
+                    <h5>Password</h5>
+                    <input type="password" class="input">
+                </div>
+            </div>
+            <a href="#">Forgot Password?</a>
+            <input type="submit" class="btn text-white" value="Login">
+        </form>
+    </div>
 </div>
 
 
-    </div> -->
-    <script type="text/javascript" src="js/main.js"></script>
+</div> -->
+<script type="text/javascript" src="js/main.js"></script>
+
 </body>
 </html>

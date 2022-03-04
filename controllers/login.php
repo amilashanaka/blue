dd9d81469d860bc3e55bd4b05adf9e2de4066b72
Amila Shanka Gunashinha <dsa.amilashanaka@gmail.com>
2022-03-04 07:17:47 +0000

    Update login.php

--- controllers/login.php
+++ controllers/login.php
@@ -14,15 +14,15 @@ $api_pass="admin@2021";
 if(user_login($u_id, $u_pass)){
     $token=get_access_token($api_user,$api_pass);
 
-    if ($_SESSION['SecKey'] == '') {
-        $_SESSION['login'] = $res['a_id'];
-        $_SESSION['login_name'] = $res['a_username'];
-        $_SESSION['login_type'] = $res['a_type'];
-        $_SESSION['login_type_name'] = getAdminType($res['a_type'], $conn);
-        $_SESSION['SecKey'] = setSecKey($res['a_id'], $conn);
-
-        header('Location: ../index.php');
-    }
+//    if ($_SESSION['SecKey'] == '') {
+//        $_SESSION['login'] = $res['a_id'];
+//        $_SESSION['login_name'] = $res['a_username'];
+//        $_SESSION['login_type'] = $res['a_type'];
+//        $_SESSION['login_type_name'] = getAdminType($res['a_type'], $conn);
+//        $_SESSION['SecKey'] = setSecKey($res['a_id'], $conn);
+//
+//        header('Location: ../index.php');
+//    }
 
     $user_type=get_user_type($u_id,$token);
 
